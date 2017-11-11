f3c4e7dbd65bb61f0d8c42273543d95137881cd7
VMatheusG <matheusxd9@hotmail.com>
2017-11-11 19:05:44 -0200

    Residuos Univariados

--- Mresiduos.R
+++ Mresiduos.R
@@ -1,54 +1,46 @@
-library(ggplot2)
-library(car)
-library(MASS)
-library(grid)
-library(gridBase)
 
-graf_resid <- function(fit=NULL, ajustado = NULL, stud_res = NULL){
-if ( !is.null(fit)) {
-  ajustado <- fitted(fit)
-  stud_res <-  studres(fit)
+
+gen.graf.resid<-function(mY,mresult,var,typeresid,wplot)
+{
+  mresiduo <- mresult$residuals
+  mbeta <- coef(mresult)
+  mX <- as.matrix(model.matrix(mresult))
+  n <- nrow(mX)
+  p <- ncol(mbeta)
+  q <- nrow(mbeta)
+  mSigma<-t(mY-mX%*%mbeta)%*%(mY-mX%*%mbeta)/(n-q)
+  if (typeresid == "univariate")
+  {
+    auxres <- diag((diag(n) - mX%*%solve(t(mX)%*%mX)%*%t(mX)))
+    mresiduo <- mresiduo/(sqrt((matrix(auxres,n,p))%*%diag(diag(mSigma))))
+  }
+  else if (typeresid == "multivariate")
+  {
+    mresiduo <- t(solve(t(chol(mSigma)))%*%t(mresiduo))
+  }
+  mfit <- fitted.values(mresult)
+  #
+  if (wplot == "diagnostics")
+  {
+    par(mfrow =c(2,2))
+    plot(mresiduo[,var],ylim=c(min(-3,min(mresiduo[,var])),max(3,max(mresiduo[,var]))),xlab="índice",ylab="resíduo studentizado")
+    abline(-2,0,lty=2)
+    abline(2,0,lty=2)
+    abline(0,0,lty=2)
+    #
+    plot(mfit[,var],mresiduo[,var],ylim=c(min(-3,min(mresiduo[,var])),max(3,max(mresiduo[,var]))),xlab="valor ajustado",ylab="resíduo studentizado")
+    abline(-2,0,lty=2)
+    abline(2,0,lty=2)
+    abline(0,0,lty=2)
+    #
+    hist(mresiduo[,var],probability=TRUE,xlab="resíduo studentizado",main="",ylab="densidade")
+    #
+    qqPlot((mresiduo[,var]),dist="norm",mean=0,sd=1,col.lines=1,grid="FALSE",xlab="quantil da N(0,1)",ylab=paste("quantil do resíduo studentizado"),cex=1.2,id.cex=1.2)
+  }
+
+  else if (wplot == "envelope")
+  {
+    par(mfrow =c(1,1))
+    qqPlot((mresiduo[,var]),dist="norm",mean=0,sd=1,col.lines=1,grid="FALSE",xlab="quantil da N(0,1)",ylab=paste("quantil do res?duo studentizado"),cex=1.2,id.cex=1.2)
+  }
 }
-  Indice <- 1:length(stud_res)
-
-p1 <- qplot(x = Indice, y = stud_res ,geom = "point") +
-    labs(y = "Residuo Studentizado",x = "Indice") +
-    theme_bw()
-
-p2 <- qplot(x = ajustado, y = stud_res ,geom = "point")  +
-   labs(y = "Residuo Studentizado",x = "Valores Ajustados")  +
-   theme_bw()
-
-p3 <-   ggplot(data.frame(stud_res),aes(stud_res,col = I("black"),fill = I("white"))) +
-   geom_histogram(binwidth = 1, aes(y=..density..)) +
-   labs( x = "Residuo Studentizado", y = "Densidade") +
-   theme_bw()
-
-plot.new()
-pushViewport(viewport(layout = grid.layout(2,3 )))
-
-#Draw ggplot 1
-pushViewport(viewport(layout.pos.col = 1,layout.pos.row = 1))
-print(p1, newpage = FALSE)
-popViewport()
-#Draw ggplot 2
-pushViewport(viewport(layout.pos.col = 2,layout.pos.row = 1))
-print(p2, newpage = FALSE)
-popViewport()
-#Draw ggplot 3
-pushViewport(viewport(layout.pos.col = 1,layout.pos.row = 2))
-print(p3, newpage = FALSE)
-popViewport()
-
-#Draw base plot
-pushViewport(viewport(layout.pos.col = 2,layout.pos.row = 2))
-par(fig = gridFIG(), new = TRUE)
-par(mar = c(2.5,2.6,0.5,0),cex.axis = 0.85,
-    cex.lab = .96,las = 1,mgp = c(1.5, .5, 0))
-qqPlot(stud_res,ylab = "Residuo Studentizado", xlab = "Quantils Teoricos",
-       dist = "norm", mean = 0, sd = 1, pch = 1,cex = 1.2, grid = "FALSE")
-popViewport()
-
-}
-
-
